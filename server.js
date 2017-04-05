82731595806b8c59ac422a3ed728c03f689bb3d0
Robert <robert.bruchhardt@gmail.com>
2017-04-05 22:32:28 +0200

    Add PROD environment variable

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ if (env =='PROD'){
   var lex = require('letsencrypt-express').create({
   // set to https://acme-v01.api.letsencrypt.org/directory in production
   //server: 'staging',
-    server: 'staging',
+    server: 'https://acme-v01.api.letsencrypt.org/directory',
 
   // If you wish to replace the default plugins, you may do so here
   //
